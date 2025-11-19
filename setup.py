e225dd2162c95984c4dc02c00b16da7db82fd9fb
HafeezOJ <32125238+HafeezOJ@users.noreply.github.com>
2025-11-19 14:16:40 -0700

    Include python 3.12 and 3.13

--- setup.py
+++ setup.py
@@ -27,6 +27,8 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.11'
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13'
     ]
 )
