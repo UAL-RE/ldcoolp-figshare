8fe63ac38ce7582d00b593d813d85fc888f3e190
HafeezOJ <32125238+HafeezOJ@users.noreply.github.com>
2025-11-19 12:01:17 -0700

    Include test function

--- tests/test_endpoint.py
+++ tests/test_endpoint.py
@@ -2,5 +2,7 @@ from ldcoolp_figshare.main import FigshareInstituteAdmin
 
 token = "token_dummy_1234567890abcdef"
 inst_admin = FigshareInstituteAdmin(token)
-api_endpoint = "get_curation_list"
-assert inst_admin.endpoint(api_endpoint) == "https://api.figshare.com/v2/account/institution/get_curation_list"
+
+def test_endpoint():
+    api_endpoint = "get_curation_list"
+    assert inst_admin.endpoint(api_endpoint) == "https://api.figshare.com/v2/account/institution/get_curation_list"
