302ae72142ad7618feafd882bc31a196fb0ba246
HafeezOJ <32125238+HafeezOJ@users.noreply.github.com>
2025-11-19 11:53:39 -0700

    Correct test output

--- tests/test_endpoint.py
+++ tests/test_endpoint.py
@@ -3,4 +3,4 @@ from ldcoolp_figshare.main import FigshareInstituteAdmin
 token = "token_dummy_1234567890abcdef"
 inst_admin = FigshareInstituteAdmin(token)
 api_endpoint = "get_curation_list"
-assert inst_admin.endpoint(api_endpoint) == "https://api.figshare.com/v2/account/get_curation_list"
+assert inst_admin.endpoint(api_endpoint) == "https://api.figshare.com/v2/account/institution/get_curation_list"
